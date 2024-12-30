1baa5a2d06b77e0b62d9b7e8068bc974361607cf
Rafał Miłosz Michałowski <58588822+Rmichalowski@users.noreply.github.com>
2024-12-30 16:11:51 +0100

    najdlwsppod delete unnecessary comment

--- najdlwsppod.c
+++ najdlwsppod.c
@@ -1,8 +1,3 @@
-// (4+1 pkt.) Zaimplementować algorytm wyszukiwania najłuższego wspólnego podciągu metodą programowania dynamicznego. Zademonstrowć działanie programu na reprezentatywnych danych testowych (zwłaszcza punkt 2).
-//
-// Wersja iteracyjna (budowanie tablicy kolejno wierszami) i wydruk jednego najdłuższego wspólnego podciągu (4 pkt.)
-//
-// Drukowanie wszystkich najdłuższych wspólnych podciągów (to wymaga znaczącej modyfikacji algorytmu). (1 pkt)
 #include <stdio.h>
 #include<string.h>
 
